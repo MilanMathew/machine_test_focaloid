5b3eef1e736670b6bc0ef66a273f52eeb358d73c
MilanMathew <milanmails@gmail.com>
2020-12-08 11:28:13 +0530

    Added ER Diagrams

--- lib/src/d03_processing/get_cleaned_data.py
+++ lib/src/d03_processing/get_cleaned_data.py
@@ -2,4 +2,4 @@ def get_dataframe():
     import pandas as pd
 
     # load cleaned csv into dataframe
-    return pd.read_csv('https://raw.githubusercontent.com/MilanMathew/machine_test_focaloid/main/lib/data/02_intermediate/matches.csv', parse_dates=['date'])
+    return pd.read_csv('https://raw.githubusercontent.com/MilanMathew/machine_test_focaloid/master/lib/data/02_intermediate/matches.csv', parse_dates=['date'])
